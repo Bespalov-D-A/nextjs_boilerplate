837400afa05d500975286429b8784133e94e6bb2
moon <yaroslav.vinnik27@gmail.com>
2025-02-05 19:59:05 +0200

    fix bugs, refactoring select component

--- src/app/contactUs/Email/components/Select/index.tsx
+++ src/app/contactUs/Email/components/Select/index.tsx
@@ -1,7 +1,15 @@
-import React, { FC } from 'react';
+'use client';
+
+import React, { FC, useState } from 'react';
+import Image from 'next/image';
+
 import { Select } from '@mantine/core';
+
 // styles
 import styles from '../Select/select.module.scss';
+// image
+import myIcon from '../../../../../../public/icons/arrowDown.svg'
+
 
 interface ITextInput {
   label: string;
@@ -11,6 +19,8 @@ interface ITextInput {
 }
 
 const CustomSelect:FC<ITextInput> = ({label, required, errorMessage, error}) => {
+  const [opened, setOpened] = useState(false);
+  console.log(opened)
   return (
     <div className={`${styles.inputWrap} ${error && styles.inputError}`}>
       <label>
@@ -19,11 +29,14 @@ const CustomSelect:FC<ITextInput> = ({label, required, errorMessage, error}) => 
         <span className={styles.errorMessage}>{error && errorMessage}</span>
       </label>
       <Select
+        rightSection={<Image src={myIcon} className={`${styles.selectArrow} ${opened && styles.selectArrowOpen}`} alt="arrow"/>}
         data={[
           { value: 'question', label: 'Question' },
           { value: 'feedback', label: 'Feedback' },
           { value: 'complaint', label: 'Complaint' },
         ]}
+        onDropdownOpen={() => setOpened(true)}
+        onDropdownClose={() => setOpened(false)}
       />
     </div>
   )
